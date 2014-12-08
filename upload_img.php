2fb275d2da919281b26539ac71cc120cf8057a61
pionnertch <elaion256@outlook.com>
2014-12-08 00:11:39 -0200

    upload change

--- upload_img.php
+++ upload_img.php
@@ -1,131 +1,58 @@
 <?php
 
-$ftp_server = "ftp.restotech.cl";
-$ftp_user_name = "u440137862";
-$ftp_user_pass = "pionner256";
-$destination_file = "/public_html/Qrmotion/images";
 
-$source_file = $_FILES["upload"]["tmp_name"];
+
 $fac = $_REQUEST['fac'];
 $code = $_REQUEST['code'];
 
+$target_dir = "https://smilepoints.cl/images/". $fac . "/";
+$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$uploadOk = 1;
+$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+
+
 $code = preg_replace('/^0+/', '',  $code);
 
-if(is_uploaded_file ( $source_file ) == false)
-{
-	echo "no se ha recibido";
-	exit;
 
-} else{
-
-};
-
-// functions 
-
-function ftp_is_dir($ftp, $dir)
-{
-    $pushd = ftp_pwd($ftp);
-
-    if ($pushd !== false && @ftp_chdir($ftp, $dir))
-    {
-        ftp_chdir($ftp, $pushd);   
-        return true;
+// Check if image file is a actual image or fake image
+if(isset($_POST["submit"])) {
+    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+    if($check !== false) {
+        echo "File is an image - " . $check["mime"] . ".";
+        $uploadOk = 1;
+    } else {
+        echo "File is not an image.";
+        $uploadOk = 0;
     }
-
-    return false;
-} 
-
-
-// set up basic connection
-$conn_id = ftp_connect($ftp_server);
-
-ftp_pasv($conn_id, true); 
-
-// login with username and password
-$login_result = ftp_login($conn_id, $ftp_user_name, $ftp_user_pass); 
-
-// check connection
-if ((!$conn_id) || (!$login_result)) { 
-    echo "FTP connection has failed!";
-    echo "Attempted to connect to $ftp_server for user" . $ftp_user_name; 
-    exit; 
+}
+// Check if file already exists
+if (file_exists($target_file)) {
+    echo "Sorry, file already exists.";
+    $uploadOk = 0;
+}
+// Check file size
+if ($_FILES["fileToUpload"]["size"] > 9000000) {
+    echo "Sorry, your file is too large.";
+    $uploadOk = 0;
+}
+// Allow certain file formats
+if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+&& $imageFileType != "gif" ) {
+    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+    $uploadOk = 0;
+}
+// Check if $uploadOk is set to 0 by an error
+if ($uploadOk == 0) {
+    echo "Sorry, your file was not uploaded.";
+// if everything is ok, try to upload file
 } else {
-    echo "Connected to" . $ftp_server  . ", for user" .  $ftp_user_name;
+    $temp = explode(".",$_FILES["file"]["name"]);
+$newfilename = $code . '.' . end($temp);
+
+    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_dir . $newfilename)) {
+        echo "The file " . basename( $_FILES["fileToUpload"]["name"]) . " has been uploaded.";
+    } else {
+        echo "Sorry, there was an error uploading your file.";
+    }
 }
-
-
-if(ftp_is_dir($conn_id, $destination_file . "/" . $fac ."/") === false){
-	
-	   ftp_mkdir( $conn_id, $destination_file . "/" . $fac . "/");
-
-};
-
- $va =  ftp_mdtm($conn_id, $destination_file . "/" . $fac . "/" . $code .  ".jpg");
-
- if($va !== -1){
-
-if(ftp_delete($conn_id, $destination_file . "/" . $fac . "/" . $code .  ".jpg")){
-
-       uploadToFtp($conn_id, $destination_file, $fac, $code, $source_file);
-         if(ftp_mdtm($conn_id, $destination_file . "/" . $fac . "/" . $code .  ".jpg") !== -1){
-
-               } else {
-
-               uploadToFtp($conn_id, $destination_file, $fac, $code, $source_file);
-           };
-} else {
-
-           uploadToFtp($conn_id, $destination_file, $fac, $code, $source_file);
-         if( ftp_mdtm($conn_id, $destination_file . "/" . $fac . "/" . $code .  ".jpg") !== 1){
-               } else {
-               uploadToFtp($conn_id, $destination_file, $fac, $code, $source_file);
-           };
-}
-
-} else {
-
-
-       uploadToFtp($conn_id, $destination_file, $fac, $code, $source_file);
-         if( ftp_mdtm( $conn_id , $destination_file . "/" . $fac . "/" . $code .  ".jpg") !== -1 ){
-               } else {
-               uploadToFtp($conn_id, $destination_file, $fac, $code, $source_file);
-           };
-
-}
-
-
-
-
-function uploadToFtp($conn_id, $destination_file, $fac, $code, $source_file){
-
-$upload = ftp_put($conn_id, $destination_file . "/" . $fac . "/" . $code .  ".jpg", $source_file , FTP_BINARY); 
-    sleep(2);
-
-if (!$upload) { 
-echo "FTP upload has failed!  " . $_FILES['upload']['error'] . " to " . $ftp_server . " as " . $destination_file;
-ftp_close($conn_id);
-
-} else {
-
-echo "Uploaded" . $source_file . " to " . $ftp_server . " as " . $destination_file;
-ftp_close($conn_id);
-}
-}
-
-
-// close the FTP stream 
-
-
-
-
-/*
- $datos = mysqli_connect('mysql.nixiweb.com', "u440137862_eqr", "PlEyAdEs", "u440137862_qr");
-
- mysqli_query($datos, "INSERT INTO PRO (PRO_FAC, PRO_CANT_VAL, PRO_DATE , PRO_DESCRIP, PRO_CODE) VALUES (" . $fac .  ", " . $cant . ", '" . date(d-m-Y) . "' , '" . $descrip . "', '" . $code . "');");
-
-*/
-
-
-
 ?>
-
