ab5a05edb1630ec974a53a5e4cc16e6109ed1843
pionnertch <elaion256@outlook.com>
2014-12-08 20:17:00 -0200

    change source upload

--- upload_img.php
+++ upload_img.php
@@ -6,7 +6,7 @@ $fac = $_REQUEST['fac'];
 $code = $_REQUEST['code'];
 
 $target_dir = "https://smilepoints.cl/images/". $fac . "/";
-$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$target_file = $target_dir . basename($_FILES["upload"]["name"]);
 $uploadOk = 1;
 $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
 
@@ -20,7 +20,7 @@ $code = preg_replace('/^0+/', '',  $code);
 
 // Check if image file is a actual image or fake image
 if(isset($_POST["submit"])) {
-    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+    $check = getimagesize($_FILES["upload"]["tmp_name"]);
     if($check !== false) {
         //echo "File is an image - " . $check["mime"] . ".";
         $uploadOk = 1;
@@ -38,7 +38,7 @@ if (file_exists($target_file)) {
     exit;
 }
 // Check file size
-if ($_FILES["fileToUpload"]["size"] > 9000000) {
+if ($_FILES["upload"]["size"] > 9000000) {
     echo "Sorry, your file is too large.";
     $uploadOk = 0;
     exit;
@@ -54,11 +54,12 @@ if ($uploadOk == 0) {
     echo "Sorry, your file was not uploaded.";
 // if everything is ok, try to upload file
 } else {
-    $temp = explode(".",$_FILES["file"]["name"]);
+
+$temp = explode(".",$_FILES["upload"]["name"]);
 $newfilename = $code . '.' . end($temp);
 
-    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_dir . $newfilename)) {
-        echo "The file " . basename( $_FILES["fileToUpload"]["name"]) . " has been uploaded.";
+    if (move_uploaded_file($_FILES["upload"]["tmp_name"], $target_dir . $newfilename)) {
+        echo "The file " . basename( $_FILES["upload"]["name"]) . " has been uploaded.";
     } else {
         echo "Sorry, there was an error uploading your file.";
     }
