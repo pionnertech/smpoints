0abef3a5d519eb91b47fd1fa28dfb2350f202400
pionnertch <elaion256@outlook.com>
2014-12-12 23:50:25 -0200

    changes in general

--- upload_img.php
+++ upload_img.php
@@ -44,8 +44,8 @@ if ($_FILES["upload"]["size"] > 9000000) {
     exit;
 }
 // Allow certain file formats
-if($imageFileType != "JPG" && $imageFileType != "PNG" && $imageFileType != "JPEG"
-&& $imageFileType != "GIF" ) {
+if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+&& $imageFileType != "gif" ) {
     echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
     $uploadOk = 0;
 }
