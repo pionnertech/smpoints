2b3d5317b96af6cba5870f3998805cc837870b3c
pionnertch <elaion256@outlook.com>
2014-12-14 22:23:24 -0200

    er_val

--- restotechdelta.php
+++ restotechdelta.php
@@ -2178,9 +2178,10 @@ function resetBeta(){
   $('#errado').css({ color : "white"});
   $("#garID").val('');
 
-   document.querySelector("#contenedor-promos").style.top ="-105px";
+   document.querySelector("#contenedor-promos").style.top ="-107px";
    $("#contenedor-promos").slickGoTo(1);
-  // document.querySelector(".cdown").style.top ="8em";
+   document.querySelector(".cdown").style.top ="8em";
+   document.querySelector('#contenedor-promos').style.right= "-7.9%"; 
 
 
 }
