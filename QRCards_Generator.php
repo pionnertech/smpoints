161bad599db0695652e0ead33405988789b282f2
pionnertch <elaion256@outlook.com>
2014-12-16 14:32:16 -0200

    general changes

--- QRCards_Generator.php
+++ QRCards_Generator.php
@@ -98,7 +98,7 @@ background-color:#6BA446;
     margin: auto;
 left:0; right:0;
 top:0; bottom:0;
-margin-top:10em;
+margin-top:20em;
     background-color: #fff;
     display: block;
     height: 200px;
@@ -135,7 +135,7 @@ html, body {
 
 footer{
 
-    margin-bottom: 200%;
+    margin-bottom: 105%;
 }
 
 .keynumber{
