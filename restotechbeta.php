36288a2ff0e99aa2db0d1af26699ec36db99a4e4
pionnertch <elaion256@outlook.com>
2014-12-17 10:43:35 -0200

    hola

--- restotechbeta.php
+++ restotechbeta.php
@@ -897,7 +897,7 @@ border-radius: 15px;
   color: white; 
   font-weight: 800;
   width: 30em;
-  margin:  0 21% 0 48%;
+  margin:  0 21% 0 47%;
   background: #ff3019; /* Old browsers */
   background: -moz-linear-gradient(top,  #ff3019 0%, #cf0404 100%); /* FF3.6+ */
   background: -webkit-gradient(linear, left top, left bottom, color-stop(0%,#ff3019), color-stop(100%,#cf0404)); /* Chrome,Safari4+ */
