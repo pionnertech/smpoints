476aad717e9559bfd736655050187e88c703c2bf
pionnertch <elaion256@outlook.com>
2014-12-19 15:08:34 -0200

    cambio a condicion ticket number

--- inscription.php
+++ inscription.php
@@ -14,15 +14,14 @@ $datos = mysqli_connect('localhost', "root", "D1sjjDlvD0", "SM_usr10000");
 $checker = mysqli_query($datos, "SELECT STR_ID FROM STORAGE WHERE STR_USR_QR = '" . $codigo . "'" );
 
 if(mysqli_num_rows($checker) !== 0){
-
 echo '403';
 exit;
 } 
 
+
 $vari_able = mysqli_query($datos, "SELECT QR_VAL FROM QR WHERE QR_VAL = '" . $codigo . "';");
 
 if(mysqli_num_rows($vari_able) === 0 ){
-
 echo '0';
 exit;
 
@@ -31,9 +30,8 @@ exit;
 
 $result2 = mysqli_query($datos, "INSERT INTO STORAGE (STR_USR_QR, STR_USR_BA_SCORE, STR_FAC_CODE) VALUES ('" . $codigo ."', 1 ," . $fac .")");
 $result = mysqli_query($datos, "INSERT INTO USER (USR_NAME, USR_EMAIL, USR_QR, USR_DATE_ING) VALUES ('" . $name . "', '" . $email . "', '" . $codigo . "' , '" . date("d-m-Y") . "');");
-$result3 = mysqli_query($datos,"INSERT INTO TRAFFIC (TRF_USR_QR, TRF_DATE, TRF_FAC_CODE, TRF_BA_SCORE_WL, TRF_PRO_STATE, TRF_TABLE) VALUES ('" . $codigo . "' , '" . $fecha . "', " . $fac . ", 1 , 1, " . $table . ")" );
 
-if (!$result && !$result2 && !$result3){
+if (!$result && !$result2){
 
    echo 5;
 
