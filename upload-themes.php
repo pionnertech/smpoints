9d1b148f7866596011d19bf73dc43fef9c1db51f
pionnertch <elaion256@outlook.com>
2014-12-23 20:46:24 -0200

    set secure portocol

--- upload-themes.php
+++ upload-themes.php
@@ -1,89 +1,70 @@
 <?php
 
-
-$ftp_server = "ftp.restotech.cl";
-$ftp_user_name = "u440137862";
-$ftp_user_pass = "pionner256";
-$destination_file = "/public_html/Qrmotion/images";
-
-$source_file1 = $_FILES["background-theme"]["tmp_name"];
-$source_file2 = $_FILES["background-client"]["tmp_name"];
-
 $fac = $_REQUEST['fac'];
 
-// set up basic connection
-$conn_id = ftp_connect($ftp_server);
-
-ftp_pasv($conn_id, true); 
-
-// login with username and password
-$login_result = ftp_login($conn_id, $ftp_user_name, $ftp_user_pass); 
-
-// chack connection and login
-if ((!$conn_id) || (!$login_result)) { 
-    echo "FTP connection has failed!";
-    echo "Attempted to connect to $ftp_server for user" . $ftp_user_name; 
-    exit;
-
-} else {
-
-    echo "Connected to" . $ftp_server  . ", for user" .  $ftp_user_name;
-}
+$target_dir = "/var/www/html/images/". $fac . "/themes/";
+$target_file = $target_dir . basename($_FILES["background-client"]["name"]);
+$uploadOk = 1;
+$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 
 
-// if dir was created...
-if(ftp_is_dir($conn_id, $destination_file . "/" . $fac ) === false){
-	   ftp_mkdir( $conn_id, $destination_file . "/" . $fac );
-        ftp_mkdir( $conn_id, $destination_file . "/" . $fac . "/themes");
-
-};
+$code = preg_replace('/^0+/', '',  $code);
 
 
-if ($source_file1 !== false){
-$upload1 = ftp_put($conn_id, $destination_file . "/" . $fac . "/themes/background-theme.png", $source_file1 , FTP_BINARY); 
-}
+// Check if image file is a actual image or fake image
+if(isset($_POST["submit"])) {
+    $check = getimagesize($_FILES["background-client"]["tmp_name"]);
+    if($check !== false) {
+        //echo "File is an image - " . $check["mime"] . ".";
+        $uploadOk = 1;
 
-if ($source_file2 !== false){
-$upload2 = ftp_put($conn_id, $destination_file . "/" . $fac . "/themes/background-client.png", $source_file2 , FTP_BINARY); 
-}
-
-if (!$upload1 || !$upload2) { 
-
-echo "FTP upload has failed!  " . $_FILES['upload']['error'] . " to " . $ftp_server . " as " . $destination_file;
-
-ftp_close($conn_id);
-
-} else {
-
-if ($source_file1 !== false){
-    echo "Uploaded" . $source_file1 . " to " . $ftp_server . " as " . $destination_file;
-};
-if ($source_file1 !== false){
-    echo "Uploaded" . $source_file2 . " to " . $ftp_server . " as " . $destination_file;
-};
-
-
-ftp_close($conn_id);
-
-}
-
-// close the FTP stream 
-
-
-
-
-function ftp_is_dir($ftp, $dir)
-{
-    $pushd = ftp_pwd($ftp);
-
-    if ($pushd !== false && @ftp_chdir($ftp, $dir))
-    {
-        ftp_chdir($ftp, $pushd);   
-        return true;
+    } else {
+        echo "File is not an image.";
+        $uploadOk = 0;
+        exit;
     }
+}
 
-    return false;
+//check if directory exist 
+
+if(!is_dir($target_dir)){
+  mkdir($target_dir, 0775);
 } 
 
+// Check if file already exists
+if (file_exists($target_file)) {
+    echo "Sorry, file already exists.";
+    $uploadOk = 0;
+    exit;
+}
+// Check file size
+if ($_FILES["background-client"]["size"] > 9000000) {
+    echo "Sorry, your file is too large.";
+    $uploadOk = 0;
+    exit;
+}
+// Allow certain file formats
+if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+&& $imageFileType != "gif" ) {
+    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+    $uploadOk = 0;
+}
+// Check if $uploadOk is set to 0 by an error
+if ($uploadOk == 0) {
+    echo "Sorry, your file was not uploaded.";
+// if everything is ok, try to upload file
+} else {
+
+
+$newfilename = $code . '.jpg';
+
+    if (move_uploaded_file($_FILES["background-client"]["tmp_name"], $target_dir . $newfilename)) {
+        echo "The file " . basename($_FILES["background-client"]["tmp_name"]) . " has been uploaded.";
+    } else {
+        echo "Sorry, there was an error uploading your file.";
+    }
+}
 ?>
+
+
 
