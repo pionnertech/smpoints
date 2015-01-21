29a8dceb6c11ec40d131931175ae17e701893637
pionnertch <elaion256@outlook.com>
2015-01-21 19:36:01 -0200

    adjustment of php session_start

--- convert.php
+++ convert.php
@@ -1,8 +1,4 @@
-
-<?php 
-
-session_start();
-
+<?php session_start();
 
 if(isset($_POST["U"]))
 
--- system.php
+++ system.php
@@ -1,7 +1,4 @@
-<?php
-
-session_start();
-
+<?php session_start();
 
 if (isset($_SESSION['TxtUser'])) {
 
