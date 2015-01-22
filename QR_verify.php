a7d1b47fc3d091dd5487f2673350e11053a55aa5
pionnertch <elaion256@outlook.com>
2015-01-22 20:52:28 -0200

    passe

--- QR_verify.php
+++ QR_verify.php
@@ -7,7 +7,7 @@
 
    $query = mysqli_query($datos, "SELECT QR_ID FROM QR WHERE QR_VAL = '" . $val . "'" );
 
-   if(mysqli_num_rows($query) !== 0){
+   if(mysqli_num_rows($query) > 0){
      echo "repetido!";
    } else {
    	mysqli_query($datos, "INSERT INTO QR(QR_VAL) VALUES ('" . $val . "');");
